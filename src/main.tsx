837d973feb0cf8e8fb505f42880e0cb32bf19376
Sazz07 <sazzad.hossain882@gmail.com>
2025-04-03 22:57:24 +0600

    feat(redux): add Redux store, auth slice, and base API setup
    Integrate Redux for state management with a persistent auth slice and base API configuration. This includes setting up the store, auth slice, and API with token refresh functionality. Additionally, update the main entry point to include Redux provider and persist gate.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './App.css';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store.ts';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Toaster } from 'sonner';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+    <Toaster position='top-center' visibleToasts={1} />
   </StrictMode>
 );
